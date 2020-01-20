0073f754e3b1964a5bf3c7dedbef01eb8aa6f927
Dayvvo <Dayvvo@users.github.com>
2020-01-20 11:56:01 +0100

    Web Portfolio

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let  throttle= document.querySelector('#ham');
+
+throttle.addEventListener('click',function () {
+    let rightbar=document.querySelector('#navbar');
+    rightbar.classList.toggle('navslide');    
+})
